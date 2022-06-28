4dc0d3b114726cc65b8a9f3364cccf843a4ab096
dilhanpathum <93417540+dilhanpathum@users.noreply.github.com>
2022-06-28 22:05:30 +0530

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,14 @@
+
+package com.java.homework;
+
+public class hw1 {
+    
+    public static void main(String args[]) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+       
+    }
+}
